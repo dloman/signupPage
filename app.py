d7a4dc3d671e3a5a4c82271ca72783aa15299071
EC2 Default User <ec2-user@ip-172-26-2-160.us-west-2.compute.internal>
2025-05-14 01:14:52 +0000

    updaded for donation website

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import request
 import braintree
 import datetime
 import os
+import json
 
 app = flask.Flask(__name__)
+total_donated = 0.0
+try:
+    with open('donationTotal.json', 'r') as openfile:
+        total_donated = float(json.load(openfile)["total_donated"])
+except:
+    print("An exception occurred reading total donated")
+    total_donated = 2000
 
 def get_enviroment(env):
     if env == "Production":
@@ -28,12 +37,21 @@ bt_gateway = braintree.BraintreeGateway(
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html', year = datetime.date.today().year)
+    global total_donated
+    return flask.render_template(
+              'donate.html', 
+              total_donated="{:,}".format(total_donated),
+              percent="{:}".format(int(100*total_donated/100000.)),
+              year = datetime.date.today().year)
 
 @app.route('/update_info')
 def update_info():
+    membership_type = request.args.get('membership_type')
+    amount = request.args.get('price')
     return flask.render_template(
             'update.html',
+            membership_type=membership_type,
+            price=amount,
             client_token_from_server= bt_gateway.client_token.generate(),
             year = datetime.date.today().year)
 
@@ -42,6 +60,7 @@ def update():
     collection = bt_gateway.customer.search(braintree.CustomerSearch.email == request.form.get("email"))
 
     for customer in collection.items:
+        print(f"updating cusomter {customer}")
         result = bt_gateway.customer.update(
             customer.id,
             {
@@ -61,8 +80,9 @@ def update():
                 },
                 "payment_method_nonce": request.form.get("payment_method_nonce"),
             })
+        print(f"result = {result}")
         if not result.is_success:
-            print(f"ERROR: {result.errors}")
+            print(f"ERROR: {result.message}")
             return flask.render_template('error.html')
         return flask.render_template(
                 'thanks.html',
@@ -70,6 +90,15 @@ def update():
                 thanks="Thank you for continuing to be a sustaining sbhx member.")
     print(f"ERROR: unable to find customers")
     return flask.render_template('error.html')
+
+
+@app.route('/donate', methods=['POST'])
+def donate():
+    return flask.render_template(
+            'form_donate.html',
+            price=request.form.get("amount"),
+            client_token_from_server= bt_gateway.client_token.generate(),
+            year = datetime.date.today().year)
 
 @app.route('/basic')
 def basic():
@@ -120,7 +149,10 @@ def signup():
     for card in result.customer.credit_cards:
         sub_result = bt_gateway.subscription.create({
             "payment_method_token": card.token,
-            "plan_id": get_plan_id(request.form.get("plan_id"))
+            "plan_id": get_plan_id(request.form.get("plan_id")),
+            "options": {
+                "submit_for_settlement": True
+                }
             })
         if sub_result.is_success:
             return flask.render_template(
@@ -131,10 +163,44 @@ def signup():
         print(f"ERROR: {sub_result.errors}")
     return flask.render_template('error.html')
 
+@app.route("/donation_transaction", methods=['POST'])
+def donation_transaction():
+    global total_donated
+    print(f"total amount donated = {total_donated}")
+    result = bt_gateway.transaction.sale({
+        "amount": request.form.get("amount"),
+        "payment_method_nonce": request.form.get("payment_method_nonce"),
+        "device_data": request.form.get("device_data"),
+        'billing': {
+                "first_name": request.form.get("first_name"),
+                "last_name": request.form.get("last_name"),
+                "street_address": request.form.get("address"),
+                "extended_address": request.form.get("address2"),
+                "locality": request.form.get("city"),
+                "region": request.form.get("state"),
+                "postal_code": request.form.get("zip_code"),
+            },
+        })
+    if result.is_success:
+        total_donated += float(request.form.get("amount"))
+        with open("donationTotal.json", 'w') as f:
+            json.dump({"total_donated": total_donated}, f)
+        return flask.render_template(
+                'thanks.html',
+                title="Donatation Complete!",
+                thanks="Thank you for being a sustaining SBHX Donor")
+
+    print(f"ERROR: {result} {result.errors}")
+    return flask.render_template('error.html')
+
 if __name__ == "__main__":
-    #app.static_folder = 'static'
-    app.send_static_file('favicon.ico')
-    app.send_static_file('logo.png')
+
+    app.static_folder = 'static'
+    app.send_static_file('static/favicon.ico')
+    app.send_static_file('static/logo.png')
     app.send_static_file('js/form-validation.js')
     app.send_static_file('css/form-validation.css')
     app.send_static_file('js/form.js')
+    app.wsgi_app = ProxyFix(
+            app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+            )
